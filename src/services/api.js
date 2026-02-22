84ac540375533d1c08f51c49c306d6339aaaeb84
Md Hamdan Nasir <nasirmdham@gmail.com>
2026-02-22 15:33:18 +0530

    added netlify redirect fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://campus-events-api-6flp.onrender.com/api",
+  baseURL: "https://campus-events-api-6flp.onrender.com/api", 
 });
 
 
